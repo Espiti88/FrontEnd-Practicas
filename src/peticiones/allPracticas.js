94cf633f8ad62b7b70c45ecc8c7191354ee86af0
Samuel Espitia <samuelespitia88@gmail.com>
2023-05-28 19:12:39 -0500

    Añade las peticiones al BackEnd

---
+++ src/peticiones/allPracticas.js
@@ -0,0 +1,15 @@
+export const allPracticas = async() => {
+
+    const url = 'http://localhost:8080/practicas/todos'
+    const resp = await fetch(url)
+    const data = await resp.json();
+
+    const practicaList = data.map(practica =>({
+        id: practica.id,
+        empresa: practica.empresa,
+        supervisor: practica.supervisor,
+        tarea: practica.tarea,
+        fecha: practica.fecha
+    }));
+    return practicaList;
+}
